e6ce116774df3639a1d261e7f060ddfe1ef1f8a0
Bodzio Podolak, Jr <bodzio2k@live.com>
2023-09-24 15:58:19 +0200

    Part 1; displaying hard coded data in CollectionVIew

--- Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
@@ -4,4 +4,13 @@ namespace MonkeyFinder.Model;
 
 public class Monkey
 {
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Details { get; set; }
+    public string Image { get; set; }
+    public int Population { get; set; }
+    public float Latitude { get; set; }
+    public float Longitude { get; set; }
+
+    public override string ToString() => Name;
 }
