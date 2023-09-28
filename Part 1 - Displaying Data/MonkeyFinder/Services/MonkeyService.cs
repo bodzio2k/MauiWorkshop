5caa8e67bb8eaf4916d6c1db78420b5c1937bac3
Bodzio Podolak, Jr <bodzio2k@live.com>
2023-09-28 20:48:06 +0200

    Part 2 - Retrieving data from Internet

--- Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,30 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    readonly HttpClient httpClient = new();
+    List<Monkey> monkeyList;
+    public MonkeyService()
+    {
+        ;
+    }
+    public async Task<List<Monkey>> GetMonkeysAsync()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+        var url = "https://www.montemagno.com/monkeys.json";
+
+        var response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+
+        return monkeyList;
+    }
+
 }
--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,47 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    readonly MonkeyService monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey finder".ToUpper();
+        this.monkeyService = monkeyService;
+    }
+
+    [RelayCommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+
+        IsBusy = true;
+
+        try
+        {
+            var monkeys = await monkeyService.GetMonkeysAsync();
+
+            if (monkeys.Count != 0)
+            {
+                Monkeys.Clear();
+            }
+
+            foreach (var monkey in monkeys)
+            {
+                Monkeys.Add(monkey);
+            }
+        }
+        catch (Exception ex)
+        {
+            await AppShell.Current.DisplayAlert("Error", ex.Message, "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
