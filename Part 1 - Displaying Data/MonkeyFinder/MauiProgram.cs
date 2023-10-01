0aa4cd34b0a26880adb750fb911fa951007cc541
Bodzio Podolak, Jr <bodzio2k@live.com>
2023-10-01 10:52:03 +0200

    Part 5 - Geolocation services example added

--- Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
+++ Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
@@ -24,6 +24,9 @@ public static class MauiProgram
 		builder.Services.AddTransient<DetailsPage>();
 
 		builder.Services.AddSingleton<MonkeyService>();
+		builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+		builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+		builder.Services.AddSingleton<IMap>(Map.Default);
 		
         builder.Services.AddSingleton<MonkeysViewModel>();
         builder.Services.AddTransient<MonkeyDetailsViewModel>();
--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,16 +1,22 @@
-﻿using MonkeyFinder.Services;
+﻿using Microsoft.Maui.Devices.Sensors;
+using MonkeyFinder.Services;
 
 namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
     readonly MonkeyService monkeyService;
+    readonly IConnectivity connectivity;
+    readonly IGeolocation geolocation;
+
     public ObservableCollection<Monkey> Monkeys { get; } = new();
 
-    public MonkeysViewModel(MonkeyService monkeyService)
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkey finder".ToUpper();
         this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
     }
 
     [RelayCommand]
@@ -37,6 +43,11 @@ public partial class MonkeysViewModel : BaseViewModel
 
         try
         {
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                throw new Exception("No Internet access.");
+            }
+
             var monkeys = await monkeyService.GetMonkeysAsync();
 
             if (monkeys.Count != 0)
@@ -56,6 +67,38 @@ public partial class MonkeysViewModel : BaseViewModel
         finally
         {
             IsBusy = false;
+        }
+    }
+
+    [RelayCommand]
+    async Task GetClosestMonkeyAsync()
+    {
+        if (IsBusy || Monkeys.Count() == 0)
+            return;
+
+        try
+        {
+            var req = new GeolocationRequest(GeolocationAccuracy.Default, TimeSpan.FromSeconds(30));
+
+            var location = await geolocation.GetLocationAsync(req);
+
+            if (location is null)
+                return;
+
+            var closest = Monkeys.OrderBy(m => location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Kilometers));
+
+            if (closest is null)
+                return;
+
+            await Shell.Current.DisplayAlert("Closest Monkey", closest.FirstOrDefault().Name, "OK");
+        }
+        catch (Exception ex)
+        {
+            await AppShell.Current.DisplayAlert("Error", ex.Message, "OK");
+        }
+        finally
+        {
+            ;
         }
     }
 }
