16dcdc9c1c77fbf66de094650cd21c04eb3145d2
Bodzio Podolak, Jr <bodzio2k@live.com>
2023-10-01 20:27:06 +0200

    Part 6 - Opening maps

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -3,6 +3,33 @@
 [QueryProperty("Monkey", "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
+    private readonly IMap map;
+
     [ObservableProperty]
     public Monkey monkey;
+
+    public MonkeyDetailsViewModel(IMap map)
+    {
+        this.map = map;
+    }
+
+
+    [RelayCommand]
+    public async Task OpenMapAsync()
+    {
+        try
+        {
+            MapLaunchOptions launchOptions = new MapLaunchOptions
+            {
+                Name = Monkey.Name,
+                NavigationMode = NavigationMode.None
+            };
+
+            await map.OpenAsync(Monkey.Latitude, Monkey.Longitude, launchOptions);
+        }
+        catch (Exception ex)
+        {
+            await AppShell.Current.DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
 }
