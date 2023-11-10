ed506dbb1f76bad4a4918f38a10c41528c63660b
Bodzio Podolak, Jr <bodzio2k@live.com>
2023-11-10 20:01:30 +0100

    EF added

---
+++ Part 1 - Displaying Data/MonkeyFinder/Data/MonkeyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MonkeyFinder.Data;
+
+public class MonkeyContext : DbContext
+{
+    public DbSet<Monkey> Monkeys
+    {
+        get; set;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSqlite("DataSource=Monkeys.db");
+    }
+}
